b25d134dd9cff703d75e1e857662b8d72f1a125a
Jeffrey Ilar <jilar@petzent.com>
2019-11-26 10:03:05 -0800

    Added HomeScreen and and react-navigation library

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,StyleSheet} from 'react-native'
+//zb5FIZ28Om-C6q-ADMsmMw
+
+//QXiA4-_ivCI46MB8mb1Fm3XUtRgBHSMzIA09Huv8JAokiO1UMzNJj4ZBmX3mRNkJicpBYxCb5dQ0F4-Ocxqj3vZ9vVj4k5MP3sTtCp0LzZoI9qTgqxSQi89852zcXXYx
+const SearchScreen =()=>{
+  return(
+    <View></View>
+  );
+};
+
+const styles=StyleSheet.create({});
+
+export default SearchScreen;
